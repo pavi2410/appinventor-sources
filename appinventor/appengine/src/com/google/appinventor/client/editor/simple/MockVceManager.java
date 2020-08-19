473c250e78701071ab6aa989e3fd1f29686aaa1d
Pavitra Golchha <pavitragolchha@gmail.com>
2020-08-19 23:38:58 +0530

    MVM: use UUID for mapping

--- appinventor/appengine/src/com/google/appinventor/client/editor/simple/MockVceManager.java
+++ appinventor/appengine/src/com/google/appinventor/client/editor/simple/MockVceManager.java
@@ -1,15 +1,35 @@
 package com.google.appinventor.client.editor.simple;
 
+import com.google.appinventor.client.editor.simple.components.MockComponent;
 import com.google.appinventor.client.editor.simple.components.MockVisibleExtension;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class MockVceManager {
-    private static final Map<String, MockVisibleExtension> vceInstances = new HashMap<>();
+    // mapping for uuid to iframe id
+    private static final Map<String, String> uuidToIframeMap = new HashMap<>();
 
-    public static void insert(String iframeId, MockVisibleExtension mve) {
-        vceInstances.put(iframeId, mve);
+    // maps uuid to MVE
+    private static final Map<String, MockComponentFactory> vceInstances = new HashMap<>();
+
+    public static void create(String type, String iframeId) {
+        final String uuid = genUuid();
+
+        MockComponentFactory mcf = createFactoryForMVE(type);
+        MockComponentRegistry.register(type, mcf);
+
+        uuidToIframeMap.put(uuid, iframeId);
+        vceInstances.put(uuid, mcf);
+    }
+
+    public static MockComponentFactory createFactoryForMVE(String type) {
+        return new MockComponentFactory() {
+            @Override
+            public MockComponent create(SimpleEditor editor) {
+                return new MockVisibleExtension(editor, type);
+            }
+        };
     }
 
     public static void remove(String iframeId) {
@@ -20,11 +40,14 @@ public class MockVceManager {
         return vceInstances.containsKey(iframeId);
     }
 
-    public static MockVisibleExtension getMve(String iframeId) {
-        return vceInstances.get(iframeId);
-    }
-
     public static void reset() {
         vceInstances.clear();
     }
+
+    private static native String genUuid() /*-{
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }-*/;
 }
