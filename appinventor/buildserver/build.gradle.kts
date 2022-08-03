780a1417d1c9b05840a18bec9599c97e45787b01
Pavitra Golchha <hello@pavi2410.me>
2022-08-04 01:25:28 +0530

    Add deps in buildserver

--- appinventor/buildserver/build.gradle.kts
+++ appinventor/buildserver/build.gradle.kts
@@ -24,8 +24,6 @@ repositories {
     mavenCentral()
 }
 
-println(projects.common.artifacts)
-
 dependencies {
     implementation(projects.common)
     implementation(fileTree("lib") { include("*.jar") })
@@ -33,4 +31,12 @@ dependencies {
     implementation("args4j:args4j:2.0.22")
     implementation("com.google.guava:guava:14.0.1")
     implementation("commons-io:commons-io:2.0.1")
+    implementation("org.bouncycastle:bcprov-jdk15on:1.49")
+    implementation("org.bouncycastle:bcpkix-jdk15on:1.49")
+    implementation("org.eclipse.jdt.core.compiler:ecj:4.5.1")
+    implementation("com.android.tools:common:24.3.0")
+    implementation("com.android.tools:sdk-common:24.3.0")
+    implementation("com.android.tools:sdklib:24.3.0")
+    implementation("com.android.tools.build:builder:1.3.0")
+    implementation("com.android.tools.build:builder-model:1.3.0")
 }
