9cf1217c8f84ebd42ea3bb963d9efd32474dfbd2
Pavitra Golchha <hello@pavi2410.me>
2022-08-04 13:13:02 +0530

    split components module

---
+++ appinventor/component-scripts/build.gradle.kts
@@ -0,0 +1,9 @@
+plugins {
+    java
+}
+
+dependencies {
+    implementation(projects.commonUtils)
+    implementation(projects.componentConstants)
+    implementation(projects.componentAnnotations)
+}
