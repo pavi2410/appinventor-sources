d46f259ee98e88d944026c154f665301d98664b3
Pavitra Golchha <hello@pavi2410.me>
2022-10-15 14:27:42 +0530

    Setup appengine-backend

--- appinventor/appengine-backend/build.gradle.kts
+++ appinventor/appengine-backend/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
     war
-    id("com.google.cloud.tools.appengine") version "2.4.4"
+    id("com.google.cloud.tools.appengine-standard") version "2.4.4"
 }
 
 group = "com.google.appinventor"
@@ -14,14 +14,18 @@ java {
 }
 
 appengine {
+    tools {
+        setCloudSdkHome("C:\\Program Files (x86)\\Google\\Cloud SDK\\google-cloud-sdk")
+        cloudSdkVersion = "405.0.1"
+    }
     deploy {
         version = "1"
         projectId = "your GCP project ID"
     }
-    run {
-
-    }
 }
 
 dependencies {
+    implementation(projects.appengineShared)
+    implementation("com.google.appengine:appengine-api-1.0-sdk:2.0.9")
+    providedCompile("javax.servlet:javax.servlet-api:3.1.0")
 }
